9c2bfd86ee9fd529077cdb2db13a78b5c38a4257
anggzh <45726270+anggzh@users.noreply.github.com>
2019-03-23 23:39:10 +0700

    Add files via upload

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanClassDanObject
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mahasiswa saya = new Mahasiswa();
+            Mahasiswa kamu = new Mahasiswa();
+
+            saya.Nim = "18112297";
+            saya.Nama = "Anggi Zhaputri";
+            saya.Ipk = 3.92f;
+
+            kamu.Nim = "18112298";
+            kamu.Nama = "Zhaputri";
+            kamu.Ipk = 3.92f;
+
+            saya.Registrasi();
+            saya.IsiKrs();
+
+            kamu.Registrasi();
+            kamu.IsiKrs();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp2/README.md
@@ -0,0 +1,2 @@
+"# ClassDanObjek" 
+"# ClassDanObjek" 
